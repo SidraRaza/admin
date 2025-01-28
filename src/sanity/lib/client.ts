8ba366ca5e92703a3689e1aaf67e726cc65540db
SidraRaza <160983959+SidraRaza@users.noreply.github.com>
2025-01-28 15:01:39 +0500

    first commit

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -2,9 +2,15 @@ import { createClient } from 'next-sanity';
 
 export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'qqxycy13',
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'productions',
   apiVersion: '2025-01-10', 
-  useCdn: false,
-  token: process.env.SANITY_API_TOKEN || 'skLfR6sPB8tpFeq2RZdY1rYDlacvOF5LVZniZHGtuuNhXQiKtXmilvDfmJ2Hlg982VpeB7QURG4p1WZQktcvpSjcSJO9IobzqBaPK8j8oNDYlxS5QXSfhdeKrFmDdO3KRHH1yk212lrtU0N0GyzEDeADhwXGyRF6mp30z2ExiertuQvR8mbT',  
+  useCdn: true,
+  
 });
+console.log(process.env.NEXT_PUBLIC_SANITY_DATASET);
+console.log(process.env.NEXT_PUBLIC_SANITY_PROJECT_ID);
+console.log(process.env.SANITY_API_TOKEN);
+
+
+
 
