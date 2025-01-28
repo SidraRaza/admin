c0cb2fcb6d1617437e4d2d7951976c78c4a61791
SidraRaza <160983959+SidraRaza@users.noreply.github.com>
2025-01-28 15:20:27 +0500

    first commit

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,15 +1,12 @@
 import { createClient } from 'next-sanity';
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'qqxycy13',
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'productions',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID  ,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET  ,
   apiVersion: '2025-01-10', 
   useCdn: true,
   
 });
-console.log(process.env.NEXT_PUBLIC_SANITY_DATASET);
-console.log(process.env.NEXT_PUBLIC_SANITY_PROJECT_ID);
-console.log(process.env.SANITY_API_TOKEN);
 
 
 
